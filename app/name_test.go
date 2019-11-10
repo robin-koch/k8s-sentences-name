191a205dda31bbf24c3df8f9b2003a588af413de
Jonas Vinther <jrvinther@gmail.com>
2019-11-10 22:52:25 +0100

    Unit tests added

--- app/name_test.go
+++ app/name_test.go
@@ -15,3 +15,32 @@ func TestNameCasing(t *testing.T) {
 		t.Errorf("GetName() FAILED, got %v", name)
 	}
 }
+
+func TestNameLength(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		name := GetName()
+
+		if(len(name) < 20) {
+			t.Logf("GetName() PASSED, %s length is less than 20 characters", name)
+		} else {
+			t.Errorf("GetName() FAILED, %s length is more than 20 characters", name)
+		}
+	}
+}
+
+func TestRandomnessDistribution(t *testing.T) {
+	gotName := make(map[string]int)
+
+	for i := 0; i < 100; i++ {
+		name := GetName()
+
+		gotName[name] = gotName[name] + 1
+	}
+
+	// Expect between 1 and 5 names
+	if(len(gotName) > 0 && len(gotName) < 6) {
+		t.Logf("GetName() randomness distribution PASSED")
+	} else {
+		t.Errorf("GetName() randomness distribution FAILED")
+	}
+}
