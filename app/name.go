e3fb5302aa47ad94a9109ad4d254bbb22fe8e5a1
Jonas Vinther <jrvinther@gmail.com>
2020-01-06 15:32:00 +0100

    Added prometheus metrics

--- app/name.go
+++ app/name.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 	"math/rand"
 	"time"
+	"github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+    httpReqs = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "sentence_requests_total",
+			Help: "Number of requests",
+		},
+		[]string{"type"},
+	)
 )
 
 func GetName() (string) {
@@ -15,13 +27,21 @@ func GetName() (string) {
 	return names[rand.Intn(len(names))]
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := GetName()
+func handler(httpReqs *prometheus.CounterVec) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        name := GetName()
 
-	fmt.Fprintf(w, "%s", name)
+		fmt.Fprintf(w, "%s", name)
+
+        m := httpReqs.WithLabelValues("name")
+        m.Inc()
+    }
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+	prometheus.MustRegister(httpReqs)
+
+    http.HandleFunc("/", handler(httpReqs))
+    http.Handle("/metrics", promhttp.Handler())
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
