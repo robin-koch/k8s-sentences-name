f65fc35a40e3ec3aa77cfa4b9f770936366b8144
Ubuntu <ubuntu@leipzig2-instance12.c.praqma-training.internal>
2020-01-15 10:16:31 +0000

    training

--- app/name.go
+++ app/name.go
@@ -21,7 +21,7 @@ var (
 )
 
 func GetName() (string) {
-	names := [5]string{"Graham", "John", "Terry", "Eric", "Michael", "Robin", "Martin"}
+	names := [7]string{"Graham", "John", "Terry", "Eric", "Michael", "Robin", "Martin"}
 
 	rand.Seed(time.Now().UnixNano())
 	return names[rand.Intn(len(names))]
